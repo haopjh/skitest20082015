5e3f87f0e691e59c9b354b77c10e1eac3ae4a436
Peh Jun Hao <junhao@pigeonlab.com>
2015-08-20 20:33:37 +0800

    Added code to retrieve data from a url

--- redmarttest.js
+++ redmarttest.js
@@ -1,34 +1,85 @@
 (function() {
-
-	//The number list is generated below
-	//Dimensions help to identify the size of the grid
-	var dimensions = 0;
-	var compiledList = formatNumberList();
-	var overallRoute = [];
-
-	//Get all routes possible
-	for (var i=0; i<compiledList.length; i++) {
-		for (var j=0; j<compiledList[i].length; j++) {
-			searchNextJump([],i,j);
-		}
-	}
-
-
+	var dimension = 0;
+	var compiledList = [];
 	var longestRoute = 0;
 	var steepestJump = 0;
-	//Get the longest route with steepest jump
-	for (var i=0; i<overallRoute.length; i++) {
-		var route = overallRoute[i];
-		var routeLength = route.length;
-		var routeJump = route[0] - route[routeLength-1];
+	var overallRoute = [];
 
-		if(longestRoute <= routeLength && steepestJump < routeJump) {
-			longestRoute = routeLength;
-			steepestJump = routeJump;
-		}
+	//Initiate the gathering of data
+	getData();
+
+	function getData() {
+
+		var http = require("http");
+		var url = "http://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/map.txt";
+		var numberList = "";
+		var req = http.get(url, function (res) {
+		  	res.setEncoding('utf8');
+		  	res.on('data', function (chunk) {
+		  		//Retrieves data
+		  		numberList += chunk;
+		  	});
+		  	res.on('end', function () {
+		  		//Start the entire process
+		  		formatNumberList(numberList);
+		  	});
+		}).on('error', function(e) {
+		  	console.log("Got error: " + e.message);
+		});
 	}
 
-	console.log(longestRoute, steepestJump);
+	function formatNumberList(numberList) {
+		numberList = numberList.split(" ");
+		numberList.shift();
+		numberList.shift();
+
+		// numberList = [
+		// 4,8,7,3,
+		// 2,5,9,3,
+		// 6,3,2,5,
+		// 4,4,1,6
+		// ];
+
+		dimension = numberList[0];
+
+		//Format the data into 2 dimensional data
+		for(var i=0; i<numberList.length; i++) {
+			var column = Math.floor(i/dimension);
+			if(compiledList[column]) {
+				compiledList[column].push(numberList[i]);
+			} else {
+				compiledList.push([numberList[i]]);
+			}
+		}
+
+		//Once the data is ready, run the processing method
+		runSearch();
+		console.log(longestRoute, steepestJump)
+	}
+
+
+	function runSearch() {
+
+		//Get all routes possible
+		for (var i=0; i<compiledList.length; i++) {
+			for (var j=0; j<compiledList[i].length; j++) {
+				searchNextJump([],i,j);
+			}
+		}
+
+		//Get the longest route with steepest jump
+		for (var i=0; i<overallRoute.length; i++) {
+			var route = overallRoute[i];
+			var routeLength = route.length;
+			var routeJump = route[0] - route[routeLength-1];
+
+			if(longestRoute <= routeLength && steepestJump < routeJump) {
+				longestRoute = routeLength;
+				steepestJump = routeJump;
+			}
+		}
+	}
+	// console.log(longestRoute, steepestJump);
 
 	function searchNextJump(route, tempX, tempY) {
 
@@ -73,29 +124,6 @@
 
 	function getNextJump(x, y) {
 		return compiledList[x][y];
-	}
-
-	function formatNumberList() {
-		var numberList = [
-		4,8,7,3,
-		2,5,9,3,
-		6,3,2,5,
-		4,4,1,6
-		];
-
-		//Not the best implementation
-		dimension = Math.sqrt(numberList.length);
-		var tempList = [];
-		//Format the data into 2 dimensional data
-		for(var i=0; i<dimension*dimension; i++) {
-			var column = Math.floor(i/dimension);
-			if(tempList[column]) {
-				tempList[column].push(numberList[i]);
-			} else {
-				tempList.push([numberList[i]]);
-			}
-		}
-		return tempList;
 	}
 
 	
